e77b1d6499ed515544a090952abd88fb93748fab
hunterpuu <sumasg@hotmail.com>
2022-05-31 21:49:30 +0700

    Update variable

--- main.tf
+++ main.tf
@@ -1,42 +1,42 @@
 provider "vsphere" {
   user           = var.VSPHERE_USER
   password       = var.VSPHERE_PASS
-  vsphere_server = "vc01.home.local"
+  vsphere_server = var.VSPHERE_SERVER
 
   # If you have a self-signed cert
   allow_unverified_ssl = true
 }
 
 data "vsphere_datacenter" "datacenter" {
-  name = "Datacenter"
+  name = var.VSPHERE_DC
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = "datastore1"
+  name          = var.VSPHERE_DSG
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_compute_cluster" "cluster" {
-  name          = "Cluster"
+  name          = var.VSPHERE_CLUSTER
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_network" "network" {
-  name          = "VM Network"
+  name          = var.VSPHERE_NETWORK
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 data "vsphere_virtual_machine" "template" {
-  name          = "Centos7"
+  name          = var.VSPHERE_TEMPLATE
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = "linux"
+  name             = var.VSPHERE_VMNAME
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
-  num_cpus         = 1
-  memory           = 1024
+  num_cpus         = var.VSPHERE_VCPU
+  memory           = var.VSPHERE_VRAM
   guest_id         = data.vsphere_virtual_machine.template.guest_id
   scsi_type        = data.vsphere_virtual_machine.template.scsi_type
   network_interface {
@@ -51,15 +51,15 @@ resource "vsphere_virtual_machine" "vm" {
   clone {
     template_uuid = data.vsphere_virtual_machine.template.id
     customize {
-      linux_options {
-        host_name = "linux"
-        domain    = "home.local"
+      windows_options {
+        computer_name = var.VSPHERE_COMNAME
+        workgroup    = var.VSPHERE_WK
       }
       network_interface {
-        ipv4_address = "192.168.253.15"
-        ipv4_netmask = 24
+        ipv4_address = var.VSPHERE_IP
+        ipv4_netmask = var.VSPHERE_NETMASK
       }
-      ipv4_gateway = "192.168.253.15"
+      ipv4_gateway = var.VSPHERE_GW
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -5,3 +5,73 @@ variable "VSPHERE_USER" {
 variable "VSPHERE_PASS" {
   type = string
 }
+
+variable "VSPHERE_SERVER" {
+  type = string
+  default = "vc01.home.local"
+}
+
+variable "VSPHERE_DC" {
+  type = string
+  default = "Datacenter"
+}
+
+variable "VSPHERE_CLUSTER" {
+  type = string
+  default = "Cluster"
+}
+
+variable "VSPHERE_DSG" {
+  type = string
+  default = "datastore1"
+}
+
+variable "VSPHERE_NETWORK" {
+  type = string
+  default = "VM Network"
+}
+
+variable "VSPHERE_TEMPLATE" {
+  type = string
+  default = "W2K12"
+}
+
+variable "VSPHERE_VMNAME" {
+  type = string
+  default = "WIN"
+}
+
+variable "VSPHERE_VCPU" {
+  type = number
+  default = "2"
+}
+
+variable "VSPHERE_VRAM" {
+  type = string
+  default = "4096"
+}
+
+variable "VSPHERE_COMNAME" {
+  type = string
+  default = "WIN"
+}
+
+variable "VSPHERE_WK" {
+  type = string
+  default = "HOME"
+}
+
+variable "VSPHERE_IP" {
+  type = string
+  default = "192.168.253.15"
+}
+
+variable "VSPHERE_NETMASK" {
+  type = string
+  default = "24"
+}
+
+variable "VSPHERE_GW" {
+  type = string
+  default = "192.168.253.1"
+}
